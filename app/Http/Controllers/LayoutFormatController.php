d2f032b482f35f5c6913e1a4265f1918ee34c80b
Olivier Montigny <olivier.wagon498@free.fr>
2022-03-22 11:32:26 +0100

    update handler and LayoutFormatController

--- app/Http/Controllers/LayoutFormatController.php
+++ app/Http/Controllers/LayoutFormatController.php
@@ -4,17 +4,20 @@ namespace App\Http\Controllers;
 
 use Illuminate\Routing\Controller as BaseController;
 
-class LayoutformatController extends BaseController
+class LayoutFormatController extends BaseController
 {
-    function rtl(){
-    	return view('layoutformat.rtl');
-    }
+  function rtl()
+  {
+    return view('layoutformat.rtl');
+  }
 
-    function horizontal(){
-    	return view('layoutformat.horizontal');
-    }
+  function horizontal()
+  {
+    return view('layoutformat.horizontal');
+  }
 
-    function smallmenu(){
-    	return view('layoutformat.smallmenu');
-    }
+  function smallmenu()
+  {
+    return view('layoutformat.smallmenu');
+  }
 }
