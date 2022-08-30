4c128094325e8c2661501944570a5998ced6df3d
Olivier Montigny <olivier.wagon498@free.fr>
2022-08-30 21:27:56 +0200

    dd in comment

--- resources/views/smallGroups/create.blade.php
+++ resources/views/smallGroups/create.blade.php
@@ -19,6 +19,7 @@
 
 @stop
 @section('content')
+{{-- {{dd($rallyes)}} --}}
 @if(count($rallyes) > 0)
 <div class='container'>
 {{ Form::open(['action' => 'SmallGroupsController@store', 'method' => 'GET']) }}
@@ -52,7 +53,7 @@
             <i class="zmdi zmdi-calendar"></i>
         </span>
         <input type="text" id="date" name="eventDate" class="form-control floating-label" placeholder="Date">
-        
+
     </div>
     <div class="help-info">If you want to create RED/GROUP for example and you are working on rallye(Petit Rallye), you do not need to specify a date</div>
 </div>
@@ -82,39 +83,39 @@
 <script src="{{secure_asset('assets/js/pages/forms/basic-form-elements.js')}}"></script>
 
 <script>
-    $(function() {              
+    $(function() {
            // Bootstrap DateTimePicker v4
-           
+
            $('#date').bootstrapMaterialDatePicker({ format : 'DD/MM/YYYY', weekStart : 0, time: false });
-        });      
+        });
 
     /*global $ */
     $(document).ready(function() {
       "use strict";
       $('.menu > ul > li:has( > ul)').addClass('menu-dropdown-icon');
       //Checks if li has sub (ul) and adds class for toggle icon - just an UI
-    
+
       $('.menu > ul > li > ul:not(:has(ul))').addClass('normal-sub');
-    
+
       $(".menu > ul > li").hover(function(e) {
         if ($(window).width() > 943) {
           $(this).children("ul").stop(true, false).fadeToggle(150);
           e.preventDefault();
         }
       });
-      //If width is more than 943px dropdowns are displayed on hover    
+      //If width is more than 943px dropdowns are displayed on hover
       $(".menu > ul > li").on('click',function() {
         if ($(window).width() <= 943) {
           $(this).children("ul").fadeToggle(150);
         }
       });
       //If width is less or equal to 943px dropdowns are displayed on click (thanks Aman Jain from stackoverflow)
-    
+
       $(".h-bars").on('click',function(e) {
         $(".menu > ul").toggleClass('show-on-mobile');
         e.preventDefault();
       });
       //when clicked on mobile-menu, normal menu is shown as a list, classic rwd menu story (thanks mwl from stackoverflow)
-    });    
+    });
 </script>
 @stop
